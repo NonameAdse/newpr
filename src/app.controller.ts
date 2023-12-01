c0b4d775a8b9cf444a8bfff725d2959140f41021
Chimder <127688661+Chimder@users.noreply.github.com>
2023-12-01 16:58:39 +0200

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('puss/:id')
+  getPussi(@Param('id', ParseIntPipe) id: number) {
+    return id;
+  }
+}
