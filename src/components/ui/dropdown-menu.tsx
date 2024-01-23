0a5cc56eb295c6b6d63d3e86e55071fca2dbed86
Chimder <127688661+Chimder@users.noreply.github.com>
2024-01-23 17:40:58 +0200

    wsl

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
+import { cn } from '@/shared/lib/utils'
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { CheckIcon, ChevronRightIcon, DotFilledIcon } from '@radix-ui/react-icons'
-import { cn } from '@/shared/lib/utils'
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
